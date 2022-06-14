7a611d446f6de82355c69ad9fafdd0117f9a84f9
alliepi <79091099+alliepi@users.noreply.github.com>
2022-06-13 21:35:39 -0400

    Updated RaceCard

--- src/app/shared/RACES.js
+++ src/app/shared/RACES.js
@@ -52,5 +52,95 @@ export const RACES = [
             "country_code": "US"
         },
         "logo_url": "https://d368g9lw5ileu7.cloudfront.net/races/race84148-logo.bIxTDE.png",
+    },
+    {
+        "race_id": 17976,
+        "name": "\"Friends on the 4th\" 5K",
+        "last_date": "06/26/2021",
+        "last_end_date": "07/04/2021",
+        "next_date": "06/26/2022",
+        "next_end_date": "07/04/2022",
+        "url": "https://runsignup.com/Race/ME/Winthrop/FriendsontheFourthAnnual5K",
+        "address": {
+            "street": "117 Bowdoin Street",
+            "street2": null,
+            "city": "Winthrop",
+            "state": "ME",
+            "zipcode": "04364",
+            "country_code": "US"
+        },
+        "logo_url": "https://d368g9lw5ileu7.cloudfront.net/races/race17976-logo.bGR9t9.png",
+    },
+    {
+        "race_id": 19541,
+        "name": "\"Heal & Soul\" 5K & Fun Run",
+        "last_date": "07/12/2019",
+        "last_end_date": "07/12/2019",
+        "next_date": "07/15/2022",
+        "next_end_date": "07/15/2022",
+        "url": "https://runsignup.com/Race/NC/Lenoir/BlackberryGlow5kandFunRun",
+        "address": {
+            "street": "Main St and West Ave Intersection",
+            "street2": null,
+            "city": "Lenoir",
+            "state": "NC",
+            "zipcode": "28645",
+            "country_code": "US"
+        },
+        "logo_url": "https://d368g9lw5ileu7.cloudfront.net/races/race19541-logo.bItzIl.png",
+    },
+    {
+        "race_id": 129457,
+        "name": "\"It's Ok\" Mental Health & Wellness 5K Run/Walk",
+        "last_date": null,
+        "last_end_date": null,
+        "next_date": "07/30/2022",
+        "next_end_date": "07/30/2022",
+        "url": "https://runsignup.com/Race/LA/Hammond/ItsOkMentalHealthWellness5KRunWalk",
+        "address": {
+            "street": "19325 Hipark Boulevard",
+            "street2": null,
+            "city": "Hammond",
+            "state": "LA",
+            "zipcode": "70403",
+            "country_code": "US"
+        },
+        "logo_url": "https://d368g9lw5ileu7.cloudfront.net/",
+    },
+    {
+        "race_id": 38269,
+        "name": "\"JJ5K\"",
+        "last_date": "09/25/2021",
+        "last_end_date": "09/25/2021",
+        "next_date": "09/24/2022",
+        "next_end_date": "09/25/2022",
+        "url": "https://runsignup.com/Race/NC/Stanley/CPC5KTraining",
+        "address": {
+            "street": "5481 Hickory Grove Rd",
+            "street2": null,
+            "city": "Stanley",
+            "state": "NC",
+            "zipcode": "28164",
+            "country_code": "US"
+        },
+        "logo_url": "https://d368g9lw5ileu7.cloudfront.net/races/race38269-logo.bzWhaH.png",
+    },
+    {
+        "race_id": 107544,
+        "name": "\"Just Keep Walking\" Virtual Challenge",
+        "last_date": "03/15/2021",
+        "last_end_date": "08/31/2021",
+        "next_date": "10/30/2021",
+        "next_end_date": "08/15/2022",
+        "url": "https://runsignup.com/Race/NY/Anycity/JustKeepWalking",
+        "address": {
+            "street": "Anywhere you like!",
+            "street2": null,
+            "city": "Any city",
+            "state": "NY",
+            "zipcode": "00000",
+            "country_code": "US"
+        },
+        "logo_url": "https://d368g9lw5ileu7.cloudfront.net/races/race107544-logo.bHHynj.png",
     }
 ]
--- src/features/races/RaceCard.js
+++ src/features/races/RaceCard.js
@@ -1,8 +1,26 @@
-import { Card, CardImg, Row, Col, CardTitle, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
+import { ListGroupItem, ListGroupItemHeading, ListGroupItemText, Button, Row, Col } from 'reactstrap';
+const RaceCard = ({ race }) => {
+    const { name, next_date, address, url } = race;
 
-const RaceCard = (props) => {
     return (
-        <ListGroupItemHeading>{props.race.name}</ListGroupItemHeading>
+        <Row>
+            <Col md={7}>
+                <ListGroupItemHeading>{name}</ListGroupItemHeading>
+                <ListGroupItemText>{next_date}</ListGroupItemText>
+                <ListGroupItemText>{address.city}</ListGroupItemText>
+                <ListGroupItemText>{address.state}</ListGroupItemText>
+            </Col>
+            <Col md={5}>
+                <div>
+                    <Button
+                        color="primary"
+                        href={url}
+                    >
+                        Discover this race!
+                    </Button>
+                </div>
+            </Col>
+        </Row>
     )
 }
 
